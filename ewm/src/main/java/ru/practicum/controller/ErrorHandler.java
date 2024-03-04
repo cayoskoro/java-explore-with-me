8e77b7d167db15b09130a4ad91fc9faa61b2bfe8
Arkadiy Skorobogatov <skoroara@yandex.ru>
2024-03-04 23:08:33 +0300

    feat: add ErrorHandler logic

---
+++ ewm/src/main/java/ru/practicum/controller/ErrorHandler.java
@@ -0,0 +1,84 @@
+package ru.practicum.controller;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.practicum.model.ApiError;
+
+import javax.validation.ConstraintViolationException;
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class ErrorHandler {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiError handleNotFoundException(final Exception e) {
+        return new ApiError(e.getStackTrace(),
+                e.getMessage(),
+                "Not Found",
+                HttpStatus.NOT_FOUND,
+                LocalDateTime.now());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleBadRequestException(final Exception e) {
+        return new ApiError(e.getStackTrace(),
+                e.getMessage(),
+                "Bad Request",
+                HttpStatus.BAD_REQUEST,
+                LocalDateTime.now());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ApiError handleIllegalStateException(final Exception e) {
+        return new ApiError(e.getStackTrace(),
+                e.getMessage(),
+                "Illegal State",
+                HttpStatus.CONFLICT,
+                LocalDateTime.now());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleMethodArgumentNotValidException(final Exception e) {
+        return new ApiError(e.getStackTrace(),
+                e.getMessage(),
+                "Method Argument Not Valid",
+                HttpStatus.BAD_REQUEST,
+                LocalDateTime.now());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleConstraintViolationException(final ConstraintViolationException e) {
+        return new ApiError(e.getStackTrace(),
+                e.getMessage(),
+                "Constraint Violation",
+                HttpStatus.BAD_REQUEST,
+                LocalDateTime.now());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ApiError handleDbConstraintViolationException(final DataIntegrityViolationException e) {
+        return new ApiError(e.getStackTrace(),
+                e.getMessage(),
+                "DB Constraint Violation",
+                HttpStatus.CONFLICT,
+                LocalDateTime.now());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiError handleServerError(final Throwable e) {
+        return new ApiError(e.getStackTrace(),
+                e.getMessage(),
+                "Internal Server Error",
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                LocalDateTime.now());
+    }
+}
---
+++ ewm/src/main/java/ru/practicum/model/ApiError.java
@@ -0,0 +1,15 @@
+package ru.practicum.model;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ApiError {
+    private final StackTraceElement[] errors;
+    private final String message;
+    private final String reason;
+    private final HttpStatus status;
+    private final LocalDateTime timestamp;
+}

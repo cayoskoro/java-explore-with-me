babd771113dc44bb37e36e6d114a066a620664ea
Arkady Skorobogatov <skoroara@yandex.ru>
2024-03-06 00:14:50 +0300

    feat: add Category service layer logic

--- ewm/src/main/java/ru/practicum/mapper/CategoryMapper.java
+++ ewm/src/main/java/ru/practicum/mapper/CategoryMapper.java
@@ -1,9 +1,24 @@
 package ru.practicum.mapper;
 
-import org.mapstruct.Mapper;
+import org.mapstruct.*;
 import org.mapstruct.control.DeepClone;
+import ru.practicum.dto.CategoryDto;
+import ru.practicum.dto.NewCategoryDto;
+import ru.practicum.model.Category;
+
+import java.util.Collection;
 
 @Mapper(componentModel = "spring", mappingControl = DeepClone.class)
 public interface CategoryMapper {
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    void updateCategoryFromNewCategoryDto(NewCategoryDto dto, @MappingTarget Category category);
 
+    Category convertNewCategoryDtoToEntity(NewCategoryDto dto);
+
+    CategoryDto convertToDto(Category entity);
+
+    Collection<CategoryDto> convertToDtoCollection(Collection<Category> entities);
+
+    Category clone(Category entity);
 }
--- ewm/src/main/java/ru/practicum/repository/CategoryRepository.java
+++ ewm/src/main/java/ru/practicum/repository/CategoryRepository.java
@@ -1,5 +1,6 @@
 package ru.practicum.repository;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.model.Category;
 

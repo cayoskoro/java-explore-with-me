d1e0646a88d260f14eacdb1142231d4cfe1f1653
Arkady Skorobogatov <skoroara@yandex.ru>
2024-03-06 00:51:31 +0300

    feat: add User service layer logic

--- ewm/src/main/java/ru/practicum/mapper/UserMapper.java
+++ ewm/src/main/java/ru/practicum/mapper/UserMapper.java
@@ -1,8 +1,18 @@
 package ru.practicum.mapper;
 
-import org.mapstruct.Mapper;
+import org.mapstruct.*;
 import org.mapstruct.control.DeepClone;
+import ru.practicum.dto.NewUserRequest;
+import ru.practicum.dto.UserDto;
+import ru.practicum.model.User;
+
+import java.util.Collection;
 
 @Mapper(componentModel = "spring", mappingControl = DeepClone.class)
 public interface UserMapper {
+    User convertNewUserRequestToEntity(NewUserRequest dto);
+
+    UserDto convertToDto(User entity);
+
+    Collection<UserDto> convertToDtoCollection(Collection<User> entities);
 }
--- ewm/src/main/java/ru/practicum/service/impl/UserServiceImpl.java
+++ ewm/src/main/java/ru/practicum/service/impl/UserServiceImpl.java
@@ -2,10 +2,15 @@ package ru.practicum.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.dto.NewUserRequest;
 import ru.practicum.dto.UserDto;
+import ru.practicum.exception.NotFoundException;
+import ru.practicum.mapper.UserMapper;
+import ru.practicum.model.User;
+import ru.practicum.repository.UserRepository;
 import ru.practicum.service.UserService;
 
 import java.util.Collection;
@@ -15,21 +20,39 @@ import java.util.Collection;
 @RequiredArgsConstructor
 @Slf4j
 public class UserServiceImpl implements UserService {
+    private final UserMapper userMapper;
+    private final UserRepository userRepository;
 
     @Override
     public Collection<UserDto> getAllUsers(Collection<Long> ids, int from, int size) {
-        return null;
+        PageRequest page = PageRequest.of(from > 0 ? from / size : 0, size);
+        Collection<UserDto> userDtos = userMapper.convertToDtoCollection(userRepository.findByIdIn(ids, page)
+                .getContent());
+        log.info("Запрос списка пользователей из ids = {} - {}", ids, userDtos);
+        return userDtos;
     }
 
     @Override
     @Transactional
     public UserDto addNewUser(NewUserRequest newUserRequest) {
-        return null;
+        User user = userMapper.convertNewUserRequestToEntity(newUserRequest);
+        UserDto userDto = userMapper.convertToDto(userRepository.save(user));
+        log.info("Добавлен новый пользователь - {}", userDto);
+        return userDto;
     }
 
     @Override
     @Transactional
     public void deleteUser(long userId) {
+        checkUserIfExists(userId);
+        userRepository.deleteById(userId);
+        log.info("Пользователь по id = {} удален", userId);
+    }
 
+    private void checkUserIfExists(long userId) {
+        if (userRepository.existsById(userId)) {
+            log.info("Пользователя по id = {} не существует", userId);
+            throw new NotFoundException(String.format("Пользователя по id = %d не существует", userId));
+        }
     }
 }
